f3f9621ca114447c974a585ca9ab95cd112263a9
Keaghan Brown <61689928+kbrown3687524@users.noreply.github.com>
2023-03-13 12:04:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,15 @@ setup(
     author_email='3687524@myuwc,.ac.za',
     packages=['amia'],
     install_requires=['biopython>=1.79',
-                      'pymol==2.5.0',
+                      'pymol>=2.3.0',
                       'MDAnalysis[analysis]>=2.4.0',
                       'MDAnalysisTests',
                       'pandas>=1.5.1',
-			    'numpy>=1.24.2',
-			    'scipy>=1.10.1',
-			    'matplotlib>=3.7.1',
-			    'cython>=0.29.15',
-			    'networkx>=3.0',
-			    'pandas>=1.5.3',
-			    'mdtraj>=1.9.7',
+		      'numpy>=1.24.2',
+	              'scipy>=1.10.1',
+		      'matplotlib>=3.7.1',
+		      'networkx>=3.0',
+		      'pandas>=1.5.3'
                       ],
 
     classifiers=[
