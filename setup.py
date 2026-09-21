d4349fb78ce9cfb3c2e908d274de4843e232b3d4
Keaghan Brown <3687524@myuwc.ac.za>
2026-09-21 08:59:28 +0200

    Used copilot to assist port to Windows & Mac

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='amia',
-    version='1.1.3',    
+    version='1.1.4',
     description='Automated Mutation Introduction and Analysis',
     author='Keaghan Brown',
     author_email='3687524@myuwc.ac.za',
